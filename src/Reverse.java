300c1903a2890157fa79cac65f706ce06824ff1f
Jayanthan <jayanthan03@gmail.com>
2022-12-07 09:58:10 +0530

    modified a print statement

--- src/Reverse.java
+++ src/Reverse.java
@@ -7,7 +7,7 @@ public class Reverse {
             reverse =reverse+s.charAt(i);
         }
         System.out.println("The reversed string is " + reverse);
-        System.out.println("Success");
+        System.out.println("Successfully reversed the String");
         
     }
 }
