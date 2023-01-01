c87795b27c239d408ba6886ce678a3adb9302d4b
Jayanthan <jayanthan03@gmail.com>
2023-01-01 11:59:02 +0530

    Merge check

--- src/Sample2.java
+++ src/Sample2.java
@@ -1,7 +1,10 @@
 public class Sample2 {
+
+    int a =10;
     public static void main(String[] args) {
         System.out.println("Sample2 added");
         System.out.println("Creating conflict");
         System.out.println("Resolving conflicts");
-     }
+        System.out.println("Merge Check");
+    }
 }
